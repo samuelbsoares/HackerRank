eff9258764bf11d2c0a3325e8f514c122dd766f7
Samuel Soares <samuelbsoares@gmail.com>
2016-07-12 12:40:22 -0700

    Create main.cpp

---
+++ Project-Euler/3/main.cpp
@@ -0,0 +1,41 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool isPrime(long long x){
+    if ((x == 2) || (x == 3)) { return true; }
+    if ((x == 1) || (x%2 == 0)) {return false; }
+    for (long long i = 3; i <= sqrt(x); i+=2) {
+        if (x%i == 0) { return false; }
+    }
+    
+    return true;
+}
+
+int main() {
+    int x;
+    cin >> x;
+    
+    for (int i = 0; i < x; i++) {
+        long long n;
+        cin >> n;
+        long long largestFactor = 1;
+        for (long long k = 1; k <= sqrt(n); k++) {
+            if (n % k == 0) {
+                if (isPrime(k)) {
+                    largestFactor = max(k, largestFactor);
+                }
+                if (isPrime(n/k)) {
+                    largestFactor = max(largestFactor, n/k);
+                }
+            }
+        }
+        cout << largestFactor << endl;
+    }
+    
+    return 0;
+}
