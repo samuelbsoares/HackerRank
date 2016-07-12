ed55113eab8c4c59a42f672d7cd6d8a14c3c91f6
Samuel Soares <samuelbsoares@gmail.com>
2016-07-12 12:05:01 -0700

    Create main.cpp

---
+++ C/Introduction/Input-and-Output/main.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int q, w, e;
+    cin >> q >> w>> e;
+    cout << q+w+e;
+    return 0;
+}
