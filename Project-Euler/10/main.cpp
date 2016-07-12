21a5860e0e9c05d7af4b5e92d8435eda3b378ef3
Samuel Soares <samuelbsoares@gmail.com>
2016-07-12 12:48:39 -0700

    Create main.cpp

---
+++ Project-Euler/10/main.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    long n[T];
+    long limit = 0;
+
+    for (int t = 0; t < T; ++t) {
+        cin >> n[t];
+        limit = n[t] > limit ? n[t] : limit;
+    }
+    
+    vector<long> isprime(limit+1, 1);
+    long sumprime = 0;
+
+    for (long i = 2; i < limit + 1; i++) {
+        if (isprime[i]) {
+            sumprime += i;
+            isprime[i] = sumprime;
+            for (long j = i*i; j < limit + 1; j += i) {
+                isprime[j] = 0;
+            }
+        }
+    }
+
+    for (int t = 0; t < T; ++t) {
+        long m = n[t];
+        while (isprime[m] == 0) --m;
+        cout << isprime[m] << endl;
+    }
+    
+    return 0;
+}
