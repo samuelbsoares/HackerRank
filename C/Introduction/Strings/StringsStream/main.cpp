c88db287f9ebf7d94db7a0607871529c828ea433
Samuel Soares <samuelbsoares@gmail.com>
2016-07-12 12:04:17 -0700

    Create main.cpp

---
+++ C/Introduction/Strings/StringsStream/main.cpp
@@ -0,0 +1,26 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+    stringstream ss(str);
+    vector<int> ints;
+
+    std::string temp;
+    while (std::getline(ss, temp, ','))
+        ints.push_back(std::stoi(temp));
+    
+    return ints;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
