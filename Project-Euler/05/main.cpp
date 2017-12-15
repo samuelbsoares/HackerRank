87f7892bed7ae34bf3ad7963beb4bcf24449cd60
Samuel Soares <samuelbsoares@gmail.com>
2017-12-15 13:26:46 -0800

    Update main.cpp

--- Project-Euler/05/main.cpp
+++ Project-Euler/05/main.cpp
@@ -1,31 +1,25 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-bool check(long result, int n){
-    for (int i = n; i > 0; --i){
-        if (result % i != 0){
+bool check( long result, int n ) {
+    for( int i = n; i > 0; --i ) {
+        if( result % i != 0 )
             return false;
-        }
     }
     return true;
 }
 
 int main() {
     int t;
-    cin >> t;
-    
-    for (int i = 0; i < t; ++i) {
+    cin >> t;    
+    for( int i = 0; i < t; ++i) {
         long n;
         cin >> n;
         long result;
         result = 1;
-        while (!check(result, n)){
+        while( !check( result, n ) )
             result += 1;
-        }
+
         cout << result << "\n";
     }
     
