2f6bb5d7756a8e7a8e202c95489548e35a372db1
Samuel Soares <samuelbsoares@gmail.com>
2017-12-15 13:43:18 -0800

    Update main.cpp

--- Project-Euler/09/main.cpp
+++ Project-Euler/09/main.cpp
@@ -6,25 +6,22 @@
 using namespace std;
 
 int main() {
-
-    int limit = 3000;
-    vector<long long> answer(limit, -1);
     
-    for (int a = 1; a < limit; ++a) {
-        for (int b = a; b < limit; ++b) {
-            double dC = sqrt(a*a + b*b);
-            int iC = static_cast<int>(dC);
-            if (ceil(dC) == static_cast<int>(dC)) {
-                if (a+b+iC <= 3000)
-                    answer[a + b + iC] = max(answer[a + b + iC], static_cast<long long>(a * b * iC));
-            }
+    const int LIMIT = 3000;
+    vector< long long > answer( LIMIT, -1 );
+    
+    for( int a = 1; a < LIMIT; ++a ) {
+        for( int b = a; b < LIMIT; ++b ) {
+            double dC = sqrt( a * a + b * b );
+            int iC = static_cast< int >( dC );
+            if( ceil( dC ) == static_cast< int >( dC ) && ( a + b + iC <= 3000 ) )
+                    answer[ a + b + iC ] = max( answer[ a + b + iC ], static_cast< long long >( a * b * iC ) );
         }
     }
 
     int T;
     cin >> T;
-
-    for (int t = 0; t < T; ++t) {
+    while( T-- ) {
         int N;
         cin >> N;
         cout << answer[N] << endl;
