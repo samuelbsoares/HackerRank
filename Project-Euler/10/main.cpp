3d50c7fef8241307a8f3f9a0ca70743dc37e0bb2
Samuel Soares <samuelbsoares@gmail.com>
2017-12-15 14:41:50 -0800

    Update main.cpp

--- Project-Euler/10/main.cpp
+++ Project-Euler/10/main.cpp
@@ -1,40 +1,32 @@
-#include <cmath>
-#include <cstdio>
 #include <vector>
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
 int main() {
     int T;
     cin >> T;
-    long n[T];
+    long n[ T ];
     long limit = 0;
-
-    for (int t = 0; t < T; ++t) {
-        cin >> n[t];
-        limit = n[t] > limit ? n[t] : limit;
+    for( int t = 0; t < T; ++t ) {
+        cin >> n[ t ];
+        limit = n[ t ] > limit ? n[ t ] : limit;
     }
-    
-    vector<long> isprime(limit+1, 1);
+    vector< long > isprime( limit + 1, 1 );
     long sumprime = 0;
-
-    for (long i = 2; i < limit + 1; i++) {
-        if (isprime[i]) {
+    for( long i = 2; i < limit + 1; ++i ) {
+        if( isprime[ i ] ) {
             sumprime += i;
-            isprime[i] = sumprime;
-            for (long j = i*i; j < limit + 1; j += i) {
-                isprime[j] = 0;
-            }
+            isprime[ i ] = sumprime;
+            for( long j = i * i; j < limit + 1; j += i )
+                isprime[ j ] = 0;
         }
     }
-
-    for (int t = 0; t < T; ++t) {
-        long m = n[t];
-        while (isprime[m] == 0) --m;
+    for( int t = 0; t < T; ++t ) {
+        long m = n[ t ];
+        while( isprime[m] == 0 )
+            --m;
         cout << isprime[m] << endl;
     }
-    
     return 0;
 }
