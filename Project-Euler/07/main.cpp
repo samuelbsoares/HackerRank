9feff02e9355eb052b25a9a40f9995bbb9b00639
Samuel Soares <samuelbsoares@gmail.com>
2017-12-15 13:34:42 -0800

    Update main.cpp

--- Project-Euler/07/main.cpp
+++ Project-Euler/07/main.cpp
@@ -1,40 +1,38 @@
-#include <cmath>
-#include <cstdio>
 #include <vector>
+#include <cmath>
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
 int main() {
-    const unsigned long limit = 5000000;
+    const unsigned long LIMIT = 5000000;
     int count = 0;
-    bool isComposit[limit];
-    vector<long> primes;
-    long upperlimit = (long)sqrt(limit);
+    bool isComposit[ LIMIT ];
+    vector< long > primes;
+    long upperlimit = (long) sqrt( LIMIT );
 
     int T;
     cin >> T;
-    int n[T];
+    int n[ T ];
     int max_n = 0;
-    for (int q = 0; q<T; q++) {
-        cin >> n[q];
-        max_n = max_n < n[q] ? n[q] : max_n;
+    for( int q = 0; q < T; ++q ) {
+        cin >> n[ q ];
+        max_n = max_n < n[ q ] ? n[ q ] : max_n;
     }
 
-    for (long i = 2; i <= limit; ++i) {
-        if (!isComposit[i]) {
-            count += 1;
+    for( long i = 2; i <= LIMIT; ++i ) {
+        if( !isComposit[ i ] ) {
+            count++;
             primes.push_back(i);
-            if (count > max_n) { break; }
-            for (long j = i * i; j <= limit; j += i) {
-                 if (!isComposit[j]) isComposit[j] = true;
+            if( count > max_n )
+                break;
+            for( long j = i * i; j <= LIMIT; j += i) {
+                 if ( !isComposit[j] )
+                     isComposit[j] = true;
             }
         }
     }
-    for (int q = 0; q<T; q++) {
-        cout << primes[n[q]-1] << endl;
-    }
-    
+    for( int q = 0; q < T; ++q )
+        cout << primes[ n[ q ] - 1 ] << endl;
     return 0;
 }
