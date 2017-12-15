2c820efc5196c7fd8ec10410b6ecf33e617ca622
Samuel Soares <samuelbsoares@gmail.com>
2017-12-15 13:38:23 -0800

    Update main.cpp

--- Project-Euler/08/main.cpp
+++ Project-Euler/08/main.cpp
@@ -1,37 +1,32 @@
-#include <cstdio>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <limits>
 #include <algorithm>
 
 using namespace std;
 
-long mult(string sub) {
+long mult( string sub ) {
     long ans = 1;
-    for (int i = 0; i < sub.size(); ++i) {
-        ans *= (sub[i] - '0');
-    }
+    for( int i = 0; i < sub.size(); ++i )
+        ans *= ( sub[ i ] - '0' );
     return ans;
 }
 
 int main() {
     int T;
     cin >> T;
-
-    for (int t = 0; t < T; ++t) {
+    while( T-- ) {
         short K;
         int N;
         cin >> K >> N;
-        string snumber;
-        cin >> snumber;
-        vector<long> multiples;
+        string strnumber;
+        cin >> strnumber;
+        vector< long > multiples;
         
-        for (int i = 0; i < K-N; ++i) {
-            multiples.push_back(mult(snumber.substr(i, N)));
-        }
-        cout << *max_element(multiples.begin(), multiples.end()) << endl;
+        for( int i = 0; i < K - N; ++i )
+            multiples.push_back( mult( strnumber.substr( i, N ) ) );
+        
+        cout << *max_element( multiples.begin(), multiples.end() ) << endl;
     }
-    
     return 0;
 }
