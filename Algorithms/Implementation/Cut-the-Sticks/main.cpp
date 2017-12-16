3525147101a052a31ae00be1bdb0ba50a7e68af0
Samuel Soares <samuelbsoares@gmail.com>
2017-12-15 16:36:18 -0800

    Update main.cpp

--- Algorithms/Implementation/Cut-the-Sticks/main.cpp
+++ Algorithms/Implementation/Cut-the-Sticks/main.cpp
@@ -1,29 +1,26 @@
-#include <cmath>
-#include <cstdio>
 #include <vector>
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
 int main(){
     int n;
     cin >> n;
-    vector<int> arr(n);
+    vector< int > arr( n );
     
-    for(int arr_i = 0; arr_i < n; ++arr_i){
-       cin >> arr[arr_i];
-    }
+    for( int i = 0; i < n; ++i )
+       cin >> arr[ i ];
     
-    sort(arr.begin(), arr.end());
+    sort( arr.begin(), arr.end() );
     
-    for(auto i = arr.begin(); i < arr.end(); ++i) {
+    for( auto i = arr.begin(); i < arr.end(); ++i ) {
         cout << n << endl;
         --n;
-        while(*i == *(i+1)) {
+        while( *i == *( i + 1 ) ) {
             ++i;
             --n;
         }
-    }
-    
+    }    
     return 0;
 }
