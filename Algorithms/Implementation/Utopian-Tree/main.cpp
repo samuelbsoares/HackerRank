81f94511a64185c232f0d462bf683c9f2dbbc8ce
Samuel Soares <samuelbsoares@gmail.com>
2017-12-15 16:38:41 -0800

    Update main.cpp

--- Algorithms/Implementation/Utopian-Tree/main.cpp
+++ Algorithms/Implementation/Utopian-Tree/main.cpp
@@ -1,19 +1,15 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int main(){
-    int t;
-    cin >> t;
-    for(int a0 = 0; a0 < t; a0++){
+    int T;
+    cin >> T;
+    for( int i = 0; i < T; ++i){
         int n;
         cin >> n;
-        int s(1);
-        for(int k(0); k<n; k++) {
-            s += (k % 2 == 0) ? s : 1;
+        int s( 1 );
+        for( int k = 0; k < n; ++k ) {
+            s += ( k % 2 ) ? 1 : s;
         }
         cout << s << endl;
     }
